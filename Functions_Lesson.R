0601d15c74100ca948bf2957da1a1debd9196eeb
FrancescoNeirotti <fra.neirotti@gmail.com>
2020-10-29 11:47:31 +0100

    aggiunto alcun cose

--- Functions_Lesson.R
+++ Functions_Lesson.R
@@ -2,8 +2,13 @@
 
 #Store as R object class "function": they can be nested and can be passed as argument to other functions.
 
-#
-
+myplot <- function(x, y, type = "l", ...) {
+  
+  plot(x, y, type=type, ...)
+  #all the otehr remain the same! super to personalize.
+  
+  #the same ... can be used at the begin to indicate "lot of arguments before this"
+}
 
 
 
