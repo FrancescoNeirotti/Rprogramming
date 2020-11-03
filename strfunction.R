658ab4fb61ed74bbf5cc1bf448e7204a4540ffd9
FrancescoNeirotti <fra.neirotti@gmail.com>
2020-11-03 10:30:59 +0100

    continue

---
+++ strfunction.R
@@ -0,0 +1,21 @@
+# str function
+
+
+str() #look inside R objectand answer to "what is in this object?"
+
+str(str)
+str(lm)
+
+str(airquality) #quick examination of data.
+
+
+
+
+s <- split(airquality, airquality$Month)
+str(s)
+
+
+
+
+
+
