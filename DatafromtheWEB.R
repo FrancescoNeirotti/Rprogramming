29bedfcbd459580d2022f1a3bd9a84e6b3ba81a9
FrancescoNeirotti <fra.neirotti@gmail.com>
2020-11-19 13:08:02 +0100

    OK

--- DatafromtheWEB.R
+++ DatafromtheWEB.R
@@ -1,1 +1,27 @@
 #Reading from the web
+
+#webscraping: fromHTML. You can programaticaly read.
+#beware of scraping data!
+
+#Scraping from scholar page
+
+con = url("https://scholar.google.com/citations?hl=en&user=QfYycpgAAAAJ")
+htmlcode = readLines(con)
+close(con)
+htmlcode
+
+#in this ay is a mess
+#try with XML
+
+library(XML)
+
+url <- "https://scholar.google.com/citations?hl=en&user=QfYycpgAAAAJ"
+html <- htmlTreeParse(url, useInternalNodes = T)
+
+xpathSApply(htmlcode, "/title", xmlValue)
+
+aa <- xpathSApply(html, "//td[@id='cal-citeby']", xmlValue)
+
+
+#????????????????????????
+
